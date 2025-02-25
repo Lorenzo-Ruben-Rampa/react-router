c98ac562d259cf5c3bc62902c9279110a318b4d0
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-25 15:21:47 +0100

    struttura vanilla

---
+++ src/pages/ChiSiamo.jsx
@@ -0,0 +1,4 @@
+export default function ChiSiamo() {
+    return
+    <h2>Chi Siamo</h2>
+}
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,8 @@
+export default function Homepage() {
+    return (
+        <>
+            <h2>Homepage</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati et sint nemo quam dicta vel maiores vitae, eum eligendi asperiores ab quo, mollitia praesentium quaerat libero quibusdam aliquid modi cumque!</p>
+        </>
+    );
+}
